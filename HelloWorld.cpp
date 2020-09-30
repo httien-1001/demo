f20149487ef404cccd70901da327fd758a62440b
Bongbeos PC <thoa@troy.edu>
2020-09-30 19:04:20 +0700

    -m

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,tb;
+    cout << "Hello World" << endl;
+    cout << "Nhap a: ";
+    cin >> a;
+    cout << "Nhap b: " ;
+    cin >> b;
+    tb= a+b;
+    cout <<"Sum of a and b: " << tb << endl;
+    system("pause");
+    return 0;
+}
